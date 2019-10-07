f7b4c6280af8e79dbeff032ea829ae481e75b45b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 13:06:52 -0700

    Starting creation of fucntion

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,19 @@
 # put your code here.
+
+words = {}
+log_file = open("test.txt")
+
+
+for line in log_file:
+    line.rstrip()
+    word_list = line.split(" ")
+    word_list = tuple(word_list)
+    # print(word_list)
+    
+    for word in word_list:
+        words[word] = words.get(word_list, 0) + 1
+
+print(words)
+
+
+# print(words)
